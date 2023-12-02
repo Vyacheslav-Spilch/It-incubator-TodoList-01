b681d8416577b2fe1c7e60747253ea77ff37f31b
Vyacheslav-Spilch <spil4evsky@yandex.ru>
2023-12-02 22:25:43 +0300

    first commit for todolist

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import styled from 'styled-components';
 import './App.css';
+
 
 const App = () => {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <TodoList />
+      <TodoList />
+      <TodoList />
+    </div>
   );
 }
 
+const TodoList = () => {
+  return (
+    <div>
+      <h3>What to learn</h3>
+      <div>
+        <input />
+        <button>+</button>
+        <ul>
+          <li><input type={'checkbox'} checked={true}/><span>HTML/CSS</span></li>
+          <li><input type={'checkbox'} checked={true}/><span>JS</span></li>
+          <li><input type={'checkbox'} checked={true}/><span>React</span></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
 export default App;
+
